a3bc8d9e905b18491fb5ca2372af88e63bc40044
wagner <wagncarv@outlook.com>
2022-06-24 12:30:16 -0300

    Controller bem vindo

---
+++ lib/rockelivery_web/controllers/welcome_controller.ex
@@ -0,0 +1,10 @@
+defmodule RockeliveryWeb.WelcomeController do
+  use RockeliveryWeb, :controller
+
+  def index(conn, _params) do
+    conn
+    |> put_status(:ok)
+    |> json(%{ok: "Bem-vindo!"})
+  end
+
+end
