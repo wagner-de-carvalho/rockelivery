15158e4e52c6fe3c35df2a1dba62e3f29448ab72
wagner <wagncarv@outlook.com>
2022-06-24 12:30:29 -0300

    Migração tabela users

---
+++ priv/repo/migrations/20220624150227_create_users_table.exs
@@ -0,0 +1,21 @@
+defmodule Rockelivery.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :address, :string
+      add :age, :string
+      add :cep, :string
+      add :cpf, :string
+      add :email, :string
+      add :name, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:cpf])
+    create unique_index(:users, [:email])
+  end
+end
