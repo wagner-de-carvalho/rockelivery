ff35a13b650447f521bbfeadd7904e1d859bc372
wagner <wagncarv@outlook.com>
2022-06-24 20:05:35 -0300

    Fallback controller

---
+++ lib/rockelivery_web/controllers/fallback_controller.ex
@@ -0,0 +1,11 @@
+defmodule RockeliveryWeb.FallbackController do
+  use RockeliveryWeb, :controller
+  alias RockeliveryWeb.ErrorView
+
+  def call(conn, {:error, result}) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(ErrorView)
+    |> render("400.json", result: result)
+  end
+end
