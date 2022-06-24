48721ee0a7970a5c4d0951e23fd22a7c303db490
wagner <wagncarv@outlook.com>
2022-06-24 20:14:16 -0300

    Refatoração FallbackController

--- lib/rockelivery_web/controllers/fallback_controller.ex
+++ lib/rockelivery_web/controllers/fallback_controller.ex
@@ -2,10 +2,10 @@ defmodule RockeliveryWeb.FallbackController do
   use RockeliveryWeb, :controller
   alias RockeliveryWeb.ErrorView
 
-  def call(conn, {:error, result}) do
+  def call(conn, {:error, %{status: status, result: result}}) do
     conn
-    |> put_status(:bad_request)
+    |> put_status(status)
     |> put_view(ErrorView)
-    |> render("400.json", result: result)
+    |> render("error.json", result: result)
   end
 end
