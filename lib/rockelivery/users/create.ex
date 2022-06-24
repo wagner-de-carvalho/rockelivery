5e8ebe7a951a6ff8edc80441f3f2873e372dfd78
wagner <wagncarv@outlook.com>
2022-06-24 20:13:57 -0300

    Refatoração create user

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -4,5 +4,12 @@ defmodule Rockelivery.Users.Create do
     params
     |> User.changeset()
     |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %User{} = user}), do: user
+
+  defp handle_insert({:error, error}) do
+    {:error, %{status: :bad_request, result: error}}
   end
 end
