bd6cc9240a162646c634e98fbd6e53dcb236a415
wagner <wagncarv@outlook.com>
2022-06-24 20:05:52 -0300

    Usando FallbackController

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -1,8 +1,12 @@
 defmodule RockeliveryWeb.UsersController do
-    use RockeliveryWeb, :controller
-    alias Rockelivery.User
+  use RockeliveryWeb, :controller
+  alias Rockelivery.User
+  alias RockeliveryWeb.FallbackController
+
+  action_fallback FallbackController
+
   def create(conn, params) do
-    with {:ok, %User{} = user}  <- Rockelivery.create_user(params) do
+    with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
       conn
       |> put_status(:created)
       |> render("create.json", user: user)
