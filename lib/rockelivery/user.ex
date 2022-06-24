76cf7acb032f033f9a6de8f5e8f18dc71a08703a
wagner <wagncarv@outlook.com>
2022-06-24 17:01:28 -0300

    Campo virtual

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -3,7 +3,7 @@ defmodule Rockelivery.User do
   import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
-  @required_params [:address, :age, :cep, :cpf, :email, :name, :password_hash]
+  @required_params [:address, :age, :cep, :cpf, :email, :name, :password]
 
   schema "users" do
     field :address, :string
@@ -13,12 +13,21 @@ defmodule Rockelivery.User do
     field :email, :string
     field :name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
 
-  def changeset(user \\ %__MODULE__{}, params) do
-    user
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
     |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
   end
 end
