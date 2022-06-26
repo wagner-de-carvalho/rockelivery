9b3f6b2ca56d000dbe124ef6901eb8f0dd1e0f6d
wagner <wagncarv@outlook.com>
2022-06-26 17:36:56 -0300

    Refatoração UsersView

--- lib/rockelivery_web/views/users_view.ex
+++ lib/rockelivery_web/views/users_view.ex
@@ -1,11 +1,11 @@
 defmodule RockeliveryWeb.UsersView do
-   use RockeliveryWeb, :view
-   alias Rockelivery.User
+  use RockeliveryWeb, :view
+  alias Rockelivery.User
 
-   def render("create.json", %{user: %User{} = user}) do
+  def render("create.json", %{user: %User{} = user}) do
     %{
       message: "User created!",
       user: user
     }
-   end
+  end
 end
