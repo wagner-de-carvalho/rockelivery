3aae4f4f94a6531bee67e46e2f09a5bd19398c32
wagner <wagncarv@outlook.com>
2022-06-26 17:37:50 -0300

    Refatoração fachada by_id

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -3,5 +3,5 @@ defmodule Rockelivery do
   alias Rockelivery.Users.Get, as: UsersGet
 
   defdelegate create_user(params) , to: UsersCreate, as: :call
-  defdelegate get_user(uuid) , to: UsersGet, as: :by_id
+  defdelegate get_user_by_id(uuid) , to: UsersGet, as: :by_id
 end
