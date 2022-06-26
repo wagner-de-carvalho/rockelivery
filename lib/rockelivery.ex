88cff79570ff1af8230c1212f8164cb55f2f36f3
wagner <wagncarv@outlook.com>
2022-06-26 18:45:57 -0300

    Fachada delete

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,7 +1,9 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UsersCreate
+  alias Rockelivery.Users.Delete, as: UsersDelete
   alias Rockelivery.Users.Get, as: UsersGet
 
   defdelegate create_user(params) , to: UsersCreate, as: :call
   defdelegate get_user_by_id(uuid) , to: UsersGet, as: :by_id
+  defdelegate delete_user(uuid) , to: UsersDelete, as: :call
 end
