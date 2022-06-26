46e8592e4485d9da3ea3b59b7cff4ffd1504c03c
wagner <wagncarv@outlook.com>
2022-06-26 17:35:45 -0300

    Fachada UsersGet

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UsersCreate
+  alias Rockelivery.Users.Get, as: UsersGet
 
   defdelegate create_user(params) , to: UsersCreate, as: :call
+  defdelegate get_user(uuid) , to: UsersGet, as: :by_id
 end
