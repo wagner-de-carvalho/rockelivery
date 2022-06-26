e444a76c4ae4cf0a911d05286f93a36d1b9635c8
wagner <wagncarv@outlook.com>
2022-06-26 19:00:27 -0300

    Módulo Error

---
+++ lib/rockelivery/error.ex
@@ -0,0 +1,12 @@
+defmodule Rockelivery.Error do
+  @keys [:status, :result]
+  @enforce_keys @keys
+  defstruct @keys
+
+  def build(status, result) do
+    %__MODULE__{status: status, result: result}
+  end
+
+  def build_user_not_found_error, do: build(:bad_request, "User not found")
+  def build_id_format_error, do: build(:bad_request, "Invalid id format")
+end
