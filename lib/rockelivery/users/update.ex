f691c342a2e5438bb2e2fcb3f4403a10a8fa26ed
wagner <wagncarv@outlook.com>
2022-06-26 23:26:27 -0300

    Uso de Plug em update

--- lib/rockelivery/users/update.ex
+++ lib/rockelivery/users/update.ex
@@ -1,15 +1,7 @@
 defmodule Rockelivery.Users.Update do
-  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
-  def call(%{"id" => id} = params) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, _uuid} -> update(params)
-    end
-  end
-
-  defp update(%{"id" => uuid} = params) do
+  def call(%{"id" => uuid} = params) do
     case Repo.get(User, uuid) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> do_update(user, params)
