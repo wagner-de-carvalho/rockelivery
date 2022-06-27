06b4a9affe17cf6f0d5cfca6cc8907dae40b55aa
wagner <wagncarv@outlook.com>
2022-06-26 23:25:06 -0300

    Refatoração UsersView

--- lib/rockelivery_web/views/users_view.ex
+++ lib/rockelivery_web/views/users_view.ex
@@ -9,5 +9,5 @@ defmodule RockeliveryWeb.UsersView do
     }
   end
 
-  def render("show.json", %{user: %User{} = user}), do: %{user: user}
+  def render("user.json", %{user: %User{} = user}), do: %{user: user}
 end
