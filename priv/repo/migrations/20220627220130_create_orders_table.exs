344952f25be2aa1f66bf470a8522f50eca2111c3
wagner <wagncarv@outlook.com>
2022-06-27 19:13:46 -0300

    Migration create table orders

---
+++ priv/repo/migrations/20220627220130_create_orders_table.exs
@@ -0,0 +1,15 @@
+defmodule Rockelivery.Repo.Migrations.CreateOrdersTable do
+  use Ecto.Migration
+  alias Rockelivery.{Item, User}
+
+  def change do
+    create table(:orders, primary_key: false) do
+       add :id, :binary_id, primary_key: true
+       add :comments, :string
+       add :payment_method, :payment_method
+       add :user_id, references(:users, type: :binary_id)
+
+       timestamps()
+    end
+  end
+end
