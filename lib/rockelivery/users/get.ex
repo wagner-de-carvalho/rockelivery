087488b7a260e3d81719e444df06b3b23765943d
wagner <wagncarv@outlook.com>
2022-06-26 23:26:16 -0300

    Uso de Plug em get

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,15 +1,7 @@
 defmodule Rockelivery.Users.Get do
-  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
-  def by_id(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, uuid} -> get(uuid)
-    end
-  end
-
-  defp get(uuid) do
+  def by_id(uuid) do
     case Repo.get(User, uuid) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
