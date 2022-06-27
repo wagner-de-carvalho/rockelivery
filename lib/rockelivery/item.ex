b62c12b28fca45c15748579740e1caac464a30db
wagner <wagncarv@outlook.com>
2022-06-27 17:33:01 -0300

    Módulo Item

---
+++ lib/rockelivery/item.ex
@@ -0,0 +1,26 @@
+defmodule Rockelivery.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @required_params [:category, :description, :photo, :price]
+  @items_categories [:dessert, :drink, :food]
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "items" do
+    field :category, Ecto.Enum, values: @items_categories
+    field :description, :string
+    field :photo, :string
+    field :price, :decimal
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:description, min: 6)
+    |> validate_number(:price, greater_than: 0)
+  end
+end
