06424c10656995fe15253b5152a9743bc1a09a3c
wagner <wagncarv@outlook.com>
2022-06-27 11:53:36 -0300

    Testes create user

---
+++ test/rockelivery/users/create_test.exs
@@ -0,0 +1,27 @@
+defmodule Rockelivery.Users.CreateTest do
+  use Rockelivery.DataCase, async: true
+  alias Rockelivery.{Error, User}
+  alias Rockelivery.Users.Create
+  import Rockelivery.Factory
+
+  describe "call/1" do
+    test "when all params are valid, creates a user" do
+      params = build(:user_params)
+      response = Create.call(params)
+      assert {:ok, %User{}} = response
+    end
+
+    test "when there are invalid params, returns an error" do
+      params = build(:user_params, %{password: "123", age: 16})
+      response = Create.call(params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["should be at least 6 character(s)"]
+      }
+
+      assert {:error, %Error{result: changeset, status: :bad_request}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
