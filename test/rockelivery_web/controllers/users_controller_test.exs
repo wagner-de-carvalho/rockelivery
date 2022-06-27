81c3ee84825cee8fdd4d18d3921fd5386783dab4
wagner <wagncarv@outlook.com>
2022-06-27 12:15:37 -0300

    Teste UserController create

---
+++ test/rockelivery_web/controllers/users_controller_test.exs
@@ -0,0 +1,44 @@
+defmodule RockeliveryWeb.UsersControllerTest do
+  use RockeliveryWeb.ConnCase, async: true
+  import Rockelivery.Factory
+
+  describe "create/2" do
+    test "when all params are valid, creates a user", %{conn: conn} do
+      params = build(:user_string_params)
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:created)
+
+      assert %{
+               "message" => "User created!",
+               "user" => %{
+                 "address" => "Rua ABC",
+                 "age" => 30,
+                 "cpf" => "00100200300",
+                 "email" => "fulano@mail.com",
+                 "id" => _id
+               }
+             } = response
+    end
+
+    test "when there is some error, returns the error", %{conn: conn} do
+      params = build(:user_string_params, %{"password" => "123", "name" => ""})
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{
+        "message" => %{
+          "name" => ["can't be blank"],
+          "password" => ["should be at least 6 character(s)"]
+        }
+      }
+
+      assert expected_response == response
+    end
+  end
+end
