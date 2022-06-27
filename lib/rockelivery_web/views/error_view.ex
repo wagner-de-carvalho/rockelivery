a37695d6440b2e32ee459139b6ad8011f5a150db
wagner <wagncarv@outlook.com>
2022-06-27 18:44:41 -0300

    Refatoração ErrorView

--- lib/rockelivery_web/views/error_view.ex
+++ lib/rockelivery_web/views/error_view.ex
@@ -12,10 +12,13 @@ defmodule RockeliveryWeb.ErrorView do
   defp translate_errors(changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(acc, "%{#{key}}", to_string(value))
+        String.replace(acc, "%{#{key}}", translate_value(value, msg))
       end)
     end)
   end
+
+  defp translate_value({:parameterized, Ecto.Enum, _map}, msg), do: msg
+  defp translate_value(value, _msg), do: to_string(value)
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
