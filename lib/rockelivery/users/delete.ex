71c44807ccfdca0a4de1eb9df8511b5430ba7894
wagner <wagncarv@outlook.com>
2022-06-26 23:26:07 -0300

    Uso de Plug em delete

--- lib/rockelivery/users/delete.ex
+++ lib/rockelivery/users/delete.ex
@@ -1,15 +1,7 @@
 defmodule Rockelivery.Users.Delete do
-  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
-  def call(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, uuid} -> delete(uuid)
-    end
-  end
-
-  defp delete(uuid) do
+  def call(uuid) do
     case Repo.get(User, uuid) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> Repo.delete(user)
