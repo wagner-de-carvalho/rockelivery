18c5528afec6eda46963376002f89fb48a392347
wagner <wagncarv@outlook.com>
2022-06-28 10:05:16 -0300

    Módulo order, put_assoc items

---
+++ lib/rockelivery/order.ex
@@ -0,0 +1,31 @@
+defmodule Rockelivery.Order do
+  use Ecto.Schema
+  alias Rockelivery.{Item, User}
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @required_params [:address, :comments, :payment_method, :user_id]
+  @payment_methods [:debt_card, :credit_card, :money]
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "orders" do
+    field :address, :string
+    field :comments, :string
+    field :payment_method, Ecto.Enum, values: @payment_methods
+    field :user_id, :string
+
+    many_to_many :items, Item, join_through: "orders_items"
+    belongs_to :users, User
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params, items) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> put_assoc(:items, items)
+    |> validate_length(:address, min: 10)
+    |> validate_length(:comments, min: 6)
+  end
+end
