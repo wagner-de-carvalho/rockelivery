9d327ee8a6763cf2b9138cbef5459365c8d5977f
wagner <wagncarv@outlook.com>
2022-06-29 19:28:54 -0300

    Uso do cliente viacep

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,11 +1,24 @@
 defmodule Rockelivery.Users.Create do
   alias Rockelivery.{Error, Repo, User}
+  alias Rockelivery.ViaCep.Client
 
-  def call(params) do
+  def call(%{"cep" => cep} = params) do
+    case Client.get_cep_info(cep) do
+      {:ok, cep_info} -> create_user(params, cep_info)
+      {:error, _result} = error -> error
+    end
+  end
+
+  defp create_user(params, cep_info) do
     params
+    |> add_cep_info(cep_info)
     |> User.changeset()
     |> Repo.insert()
     |> handle_insert()
+  end
+
+  defp add_cep_info(params, %{"logradouro" => address}) do
+    %{params | "address" => address}
   end
 
   defp handle_insert({:ok, %User{} = user}), do: {:ok, user}
