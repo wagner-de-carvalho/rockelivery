3facb53f86f6f7a5d588af7f26bba712b6e093c5
wagner <wagncarv@outlook.com>
2022-06-30 19:34:58 -0300

    Factory cep_info

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -38,4 +38,19 @@ defmodule Rockelivery.Factory do
       id: "7d412f02-e3cc-4603-8ce3-d431fe888401"
     }
   end
+
+  def cep_info_factory do
+    %{
+      "bairro" => "Sé",
+      "cep" => "01001-000",
+      "complemento" => "lado ímpar",
+      "ddd" => "11",
+      "gia" => "1004",
+      "ibge" => "3550308",
+      "localidade" => "São Paulo",
+      "logradouro" => "Praça da Sé",
+      "siafi" => "7107",
+      "uf" => "SP"
+    }
+  end
 end
