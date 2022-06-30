77dbf652448d4df699b11b41f3440d738b809e1f
wagner <wagncarv@outlook.com>
2022-06-30 18:30:30 -0300

    Biblioteca mox adicionada

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Rockelivery.MixProject do
       {:ex_machina, "~> 2.7.0"},
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.17"},
-      {:bypass, "~> 2.1", only: :test}
+      {:bypass, "~> 2.1", only: :test},
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 
