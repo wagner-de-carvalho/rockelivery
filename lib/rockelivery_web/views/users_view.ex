74e67c514753d881228ebe203408e2e10c4befb4
wagner-de-carvalho <wagncarv@outlook.com>
2022-07-01 00:15:32 -0300

    Exibindo token gerado

--- lib/rockelivery_web/views/users_view.ex
+++ lib/rockelivery_web/views/users_view.ex
@@ -2,10 +2,11 @@ defmodule RockeliveryWeb.UsersView do
   use RockeliveryWeb, :view
   alias Rockelivery.User
 
-  def render("create.json", %{user: %User{} = user}) do
+  def render("create.json", %{user: %User{} = user, token: token}) do
     %{
       message: "User created!",
-      user: user
+      user: user,
+      token: token
     }
   end
 
