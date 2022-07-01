289a40b8214bacc6093e07c10fb5f2fed2267fb9
wagner-de-carvalho <wagncarv@outlook.com>
2022-07-01 00:08:25 -0300

    Formatação credo

--- lib/rockelivery/via_cep/client.ex
+++ lib/rockelivery/via_cep/client.ex
@@ -1,8 +1,8 @@
 defmodule Rockelivery.ViaCep.Client do
   use Tesla
-  alias Tesla.Env
   alias Rockelivery.Error
   alias Rockelivery.ViaCep.Behaviour
+  alias Tesla.Env
 
   @behaviour Behaviour
 
