d17f13e60db79decae22e0be16a772e3ecf502ea
wagner-de-carvalho <wagncarv@outlook.com>
2022-07-01 23:51:07 -0300

    Função de autenticação de usuário

--- lib/rockelivery_web/auth/guardian.ex
+++ lib/rockelivery_web/auth/guardian.ex
@@ -1,6 +1,6 @@
 defmodule RockeliveryWeb.Auth.Guardian do
   use Guardian, otp_app: :rockelivery
-  alias Rockelivery.User
+  alias Rockelivery.{Error, User}
   alias Rockelivery.Users.Get, as: UserGet
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
@@ -13,4 +13,17 @@ defmodule RockeliveryWeb.Auth.Guardian do
 
     {:ok, user}
   end
+
+  def authenticate(%{"id" => user_id, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- UserGet.by_id(user_id),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token}
+    else
+      false -> {:error, Error.build(:unauthorized, "Please verify your credentials")}
+      error -> error
+    end
+  end
+
+  def authenticate(_), do: {:error, Error.build(:bad_request, "Invalid or missing credentials")}
 end
