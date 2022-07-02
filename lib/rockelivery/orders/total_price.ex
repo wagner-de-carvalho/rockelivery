719579ccac1a949a95743b1d8b949efc04b621f8
wagner-de-carvalho <wagncarv@outlook.com>
2022-07-02 19:34:31 -0300

    Módulo TotalPrice

---
+++ lib/rockelivery/orders/total_price.ex
@@ -0,0 +1,10 @@
+defmodule Rockelivery.Orders.TotalPrice do
+  alias Rockelivery.{Item, Order, Repo}
+
+  def calculate(items) do
+    Enum.reduce(items, Decimal.new("0.00"), &sum_prices/2)
+  end
+
+  defp sum_prices(%Item{price: price}, acc), do: Decimal.add(price, acc)
+
+end
